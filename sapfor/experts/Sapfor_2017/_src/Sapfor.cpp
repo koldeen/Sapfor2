bf0e4d60f4d0d6ad836d2088a5ba8b934c5fb527
Yashin Sergey <yashserge@gmail.com>
2018-10-17 20:42:13 +0300

    fix merge

--- sapfor/experts/Sapfor_2017/_src/Sapfor.cpp
+++ sapfor/experts/Sapfor_2017/_src/Sapfor.cpp
@@ -1491,6 +1491,9 @@ void runPass(const int curr_regime, const char *proj_name, const char *folderNam
     case CHECK_FUNC_TO_INCLUDE:
         findFunctionsToInclude(true);
         break;
+    case RESOLVE_PAR_REGIONS:
+        runAnalysis(*project, curr_regime, false);
+        // do not set break in this case
     case SUBST_EXPR_AND_UNPARSE:
         if (folderName)
             runAnalysis(*project, UNPARSE_FILE, true, "", folderName);
