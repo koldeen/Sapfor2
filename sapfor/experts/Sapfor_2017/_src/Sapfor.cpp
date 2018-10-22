ebc9777808d608dcd10244dbd164006f43716423
Alexander <alexks179@gmail.com>
2018-10-22 12:40:11 +0300

    Поменял на более правильный вариант до вливания в мастер

--- sapfor/experts/Sapfor_2017/_src/Sapfor.cpp
+++ sapfor/experts/Sapfor_2017/_src/Sapfor.cpp
@@ -1492,7 +1492,8 @@ void runPass(const int curr_regime, const char *proj_name, const char *folderNam
         break;
     case RESOLVE_PAR_REGIONS:
         runAnalysis(*project, curr_regime, false);
-        // do not set break in this case
+        runPass(UNPARSE_FILE, proj_name, folderName);
+        break;
     case SUBST_EXPR_AND_UNPARSE:
         if (folderName)
             runAnalysis(*project, UNPARSE_FILE, true, "", folderName);
