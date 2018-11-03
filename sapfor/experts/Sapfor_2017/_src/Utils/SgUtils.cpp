646459f3777e8e8254769ebb5cddfc1f9201c0e9
Yashin Sergey <yashserge@gmail.com>
2018-11-03 19:39:52 +0300

    исправление порядка вставки операторов include vol.2

--- sapfor/experts/Sapfor_2017/_src/Utils/SgUtils.cpp
+++ sapfor/experts/Sapfor_2017/_src/Utils/SgUtils.cpp
@@ -262,7 +262,7 @@ void removeIncludeStatsAndUnparse(SgFile *file, const char *fileName, const char
                             if (pos == string::npos)
                                 st->setComments((it.second.first + comments).c_str());
                             else if (comments.find(it.second.first) == string::npos)
-                                st->setComments((comments.insert(comments.find('\n') + 1, it.second.first)).c_str());
+                                st->setComments((comments.insert(comments.find('\n', pos) + 1, it.second.first)).c_str());
                         }
                         else
                             st->addComment(it.second.first.c_str());
