90acedee7c30e0e0c3d8f832d16bc03ce625458d
Yashin Sergey <yashserge@gmail.com>
2018-12-21 18:26:47 +0300

    fix merge

--- sapfor/experts/Sapfor_2017/_src/Utils/SgUtils.cpp
+++ sapfor/experts/Sapfor_2017/_src/Utils/SgUtils.cpp
@@ -1662,5 +1662,6 @@ objT& getObjectForFileFromMap(const char *fileName, map<string, objT> &mapObject
 template vector<Messages>& getObjectForFileFromMap(const char *fileName, map<string, vector<Messages>>&);
 template vector<LoopGraph*>& getObjectForFileFromMap(const char *fileName, map<string, vector<LoopGraph*>>&);
 template vector<Interval*>& getObjectForFileFromMap(const char *fileName, map<string, vector<Interval*>>&);
-template std::map<int, Gcov_info>& getObjectForFileFromMap(const char *fileName, map<string, std::map<int, Gcov_info>>&);
+template map<int, Gcov_info>& getObjectForFileFromMap(const char *fileName, map<string, std::map<int, Gcov_info>>&);
 template PredictorStats& getObjectForFileFromMap(const char *fileName, map<string, PredictorStats>&);
+template map<int, double>& getObjectForFileFromMap(const char *fileName, map<string, std::map<int, double>>&);
