564b49d158f108e89e67a876861857b7a12e9626
thmis <a.m.petro@yandex.ru>
2019-02-26 01:04:11 +0300

    fixed naming (Interval ==> SpfInterval)

--- sapfor/experts/Sapfor_2017/_src/DynamicAnalysis/createParallelRegions.h
+++ sapfor/experts/Sapfor_2017/_src/DynamicAnalysis/createParallelRegions.h
@@ -10,4 +10,4 @@
 #include <vector>
 
 float performTime(SgFile *file, SgStatement *src, std::map<int, Gcov_info> &gCovInfo, std::map<std::string, std::vector<FuncInfo*>> &funcInfo, int rec_level = 0);
-void createParallelRegions(SgFile *file, std::vector<Interval*> &fileIntervals, std::map<int, Gcov_info> &gCovInfo, std::map<std::string, std::vector<FuncInfo*>> &funcInfo);
+void createParallelRegions(SgFile *file, std::vector<SpfInterval*> &fileIntervals, std::map<int, Gcov_info> &gCovInfo, std::map<std::string, std::vector<FuncInfo*>> &funcInfo);
