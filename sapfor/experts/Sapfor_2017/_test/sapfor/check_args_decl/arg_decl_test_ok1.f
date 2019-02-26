5841810ff49603f1872888a7ccea176289bf739e
Yashin Sergey <yashserge@gmail.com>
2019-02-26 12:48:02 +0300

    предварительные версии базовых тестов

---
+++ sapfor/experts/Sapfor_2017/_test/sapfor/check_args_decl/arg_decl_test_ok1.f
@@ -0,0 +1,26 @@
+        subroutine A(i1)
+		INTEGER i1
+		   i1 = i1 + 1
+		END
+
+        subroutine B(i1)
+		INTEGER i1
+		   i1 = i1 + 1
+		END
+		
+		PROGRAM PAR_REG_TEST
+        INTEGER i1
+        INTEGER summ
+		
+		summ = 0
+		do i1 = 1, 20
+		   call A(summ)
+		enddo
+		write(*,*) 'summ = ', summ
+		
+		summ = 0
+		do i1 = 1, 20
+		   call B(summ)
+		enddo		
+        write(*,*) 'summ = ', summ
+        END
