d8bbdcac58c944a3f06999700dfa234595540c1c
thmis <a.m.petro@yandex.ru>
2019-03-29 15:20:44 +0300

    Tests added
    Tests on loops converting added

---
+++ sapfor/experts/Sapfor_2017/_test/sapfor/convert_expr_to_loop/simple_expr.f
@@ -0,0 +1,9 @@
+	PROGRAM SIMPLE_EXPR
+       
+	REAL A (10), B (10), C(10)
+		
+	A = B + C
+	C = A + B
+	B = A + A
+
+	END
---
+++ sapfor/experts/Sapfor_2017/_test/sapfor/convert_where_to_loop/simple_where.f
@@ -0,0 +1,10 @@
+	PROGRAM SIMPLE_WHERE
+       
+	REAL A (10), B (10), C(10)
+	REAL S
+	
+	WHERE (A > 0) A = 3
+	WHERE (C <= 56) C = 15
+	WHERE (B /= 0) B = S / 2
+
+	END
