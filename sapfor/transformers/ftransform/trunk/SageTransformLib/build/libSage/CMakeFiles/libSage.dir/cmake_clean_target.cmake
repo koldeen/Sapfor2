ba7a1c6b3c867001d495f8b0a6618768526811f4
ALEXks <alexks179@gmail.com>
2019-05-12 21:29:49 +0300

    удаление ненужного

--- sapfor/transformers/ftransform/trunk/SageTransformLib/build/libSage/CMakeFiles/libSage.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "liblibSage.a"
-)
--- sapfor/transformers/ftransform/trunk/SageUtils/test/SageTransformUtilsTest/swapWithLexPrev.expected.f90
+++
@@ -1,7 +0,0 @@
-      program loopreorderertest
-      print *, "base statement"
-      print *, "stmt that goes higher"
-      print *, "stmt that goes lower"
-      print *, "unused stmt"
-      end
-
