71c38eccbd146e356d9f6f3c455f92acd140418a
ALEXks <alexks179@gmail.com>
2019-12-28 10:33:58 +0300

    переместил тесты в нужное место

--- sapfor/experts/Sapfor_2017/_test/jacparam.h
+++
@@ -1,4 +0,0 @@
-        double precision maxepsd,maxeps
-        parameter (nxd=512,nyd=512,nzd=512)
-        parameter(maxepsd = 0.1,  itmaxd=100)
-        DOUBLE PRECISION, ALLOCATABLE, DIMENSION (:,:,:) :: pa,pb
