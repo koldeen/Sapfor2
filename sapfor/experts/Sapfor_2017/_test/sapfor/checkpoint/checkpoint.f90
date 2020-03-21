4eba602d58a9b0e6f652912ac08821868fdfeb14
Yashin Sergey <yashserge@gmail.com>
2020-03-21 22:10:35 +0300

    add test file

---
+++ sapfor/experts/Sapfor_2017/_test/sapfor/checkpoint/checkpoint.f90
@@ -0,0 +1,103 @@
+program SRSA4
+  call srsa43
+end
+
+subroutine srsa43     
+  parameter( N = 6,M=8,K=8,L=6, PN = 2,NL=1000)
+
+!$SPF CHECKPOINT(INTERVAL(TIME,10),VARLIST(A,B,C),EXCEPT(AA,D))
+!$SPF CHECKPOINT(VARLIST(A,B,C),EXCEPT(AA,D))
+  integer  A(N,M,K,L), B(N,M,K,L), C(N,M,K,L), D(N,M,K,L)
+  integer  AA(N,M,K)
+  integer nloopi,nloopj,nloopii,nloopjj,ttt,ttt1 
+  character*9 tname
+  
+!$SPF CHECKPOINT(INTERVAL(TIME,10),FILES_COUNT(4),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+  tname='srsa43'
+  NNL=NL    
+  call serial4(C,N,M,K,L,NNL)
+  nloopi=NL
+  nloopj=NL
+  nloopii=NL
+  nloopjj=NL
+!$SPF CHECKPOINT(INTERVAL(TIME,ITER),FILES_COUNT(4,5),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL(ITER,TIME),FILES_COUNT(),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL(ITER,TIME),FILES_COUNT,VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL(ITER,-1),FILES_COUNT(555),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+  do i=1,N
+    do j=1,M
+      do ii=1,K
+        do jj=1,L
+          A(i,j,ii,jj) = NL+i+j+ii+jj
+        enddo
+      enddo
+    enddo
+  enddo
+!$SPF CHECKPOINT(INTERVAL(ITER),FILES_COUNT(555),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL(-1),FILES_COUNT(555),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL(),FILES_COUNT(555),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+!$SPF CHECKPOINT(INTERVAL,FILES_COUNT(555),VARLIST(A,B,C),TYPE(ASYNC),EXCEPT(AA,D))
+  do  i=1,N
+    do  j=1,M
+      do ii=1,K
+        ttt = ttt
+        do jj=1,L
+          ttt = A(i,j,ii,1)+A(i,j,ii,2)
+          ttt1=AA(i,j,ii)
+        enddo
+        ttt=ttt
+      enddo
+    enddo
+  enddo
+  
+  do  i=1,N
+    do  j=1,M
+      do  ii=1,K
+        do  jj=1,L
+          if (B(i,j,ii,jj).ne.(C(i,j,ii,1)+C(i,j,ii,2))) then
+            nloopi=min(nloopi,i)
+            nloopj=min(nloopj,j)
+            nloopii=min(nloopii,ii)
+            nloopjj=min(nloopjj,jj)
+          endif
+        enddo
+        ttt = ttt
+      enddo
+    enddo
+  enddo
+
+  if (nloopi .eq.NL) then     
+    call ansyes(tname)
+  else
+    call ansno(tname)
+  endif
+
+end
+
+
+subroutine serial4(AR,N,M,K,L,NL)     
+  integer AR(N,M,K,L)
+  integer NL 
+
+  do i=1,N
+    do j=1,M
+      do ii=1,K
+        do jj=1,L
+10        AR(i,j,ii,jj) = NL+i+j+ii+jj
+        enddo
+      enddo
+    enddo
+  enddo
+
+end
+
+
+subroutine ansyes(name)
+  character*9 name
+  print *,name,'  -  complete'
+end
+
+subroutine ansno(name)
+  character*7 name
+  print *,name,'  -  ***error'
+end
