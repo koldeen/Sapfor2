225f2b68686b678a69a59e81db5d322b0662171d
ALEXks <alexks179@gmail.com>
2020-07-29 11:44:02 +0300

    svn 7241

--- sapfor/experts/Sapfor_2017/_src/LoopAnalyzer/shadow.h
+++ sapfor/experts/Sapfor_2017/_src/LoopAnalyzer/shadow.h
@@ -7,73 +7,119 @@
 
 struct FuncInfo;
 struct LoopGraph;
+struct ShadowNode;
+
+typedef enum node : int { PARALLEL_DIR = 0, FUNCTION_CALL, PROCEDURE_CALL, STOP, RETURN, START, END } nodeType;
+
+struct ShadowElement
+{
+    std::vector<std::pair<int, int>> bounds;
+    std::map<FuncInfo*, std::string> origNameByProc;
+    bool corner;
+
+    ShadowElement() { }
+    ShadowElement(const std::vector<std::pair<int, int>>& bounds,
+                  const std::pair<FuncInfo*, std::string>& origName, 
+                  bool corner) : bounds(bounds), corner(corner)
+    {
+        origNameByProc[origName.first] = origName.second;
+    }
+
+    bool operator==(const ShadowElement& left) const
+    {
+        return bounds == left.bounds && origNameByProc == left.origNameByProc && corner == left.corner;
+    }
+};
+
+struct NextNode
+{
+    ShadowNode* shNode;
+    std::set<DIST::Array*> writeTo;
+    bool isBackWard;
+
+    NextNode() { }
+    NextNode(ShadowNode* shNode, const std::set<DIST::Array*>& writeTo, bool isBackWard = false)
+        : shNode(shNode), writeTo(writeTo), isBackWard(isBackWard)
+    { }
+
+    bool operator==(const NextNode& left) const
+    {
+        return (shNode == left.shNode) && (writeTo == left.writeTo);
+    }
+
+    bool operator<(const NextNode& left) const
+    {
+        return shNode < left.shNode;
+    }
+};
+
+struct PrevNode
+{
+    ShadowNode* shNode;
+    bool* isBackWard;
+
+    PrevNode() { }
+    PrevNode(ShadowNode* shNode, bool* isBackWard = NULL)
+        : shNode(shNode), isBackWard(isBackWard)
+    { }
+
+    bool operator==(const PrevNode& left) const
+    {
+        return shNode == left.shNode;
+    }
+};
 
 struct ShadowNode
 {
-    //parallel directive
-    void *dir;
+    void *info;
+    nodeType type;
+
     //file and line
     std::pair<FuncInfo*, int> location;
 
-    //                                                        bounds
-    std::vector<std::pair<DIST::Array*, std::vector<std::pair<int, int>>>> shadows;
-    std::set<DIST::Array*> corners;
+    std::map<DIST::Array*, std::vector<ShadowElement>> shadows;
+    std::map<DIST::Array*, std::vector<ShadowElement>> newShadows;
 
-    //                                                        bounds
-    std::vector<std::pair<DIST::Array*, std::vector<std::pair<int, int>>>> newShadows;
-    std::set<DIST::Array*> newCorners;
+    std::vector<NextNode> next;
+    std::vector<PrevNode> prev;
 
-    std::vector<std::pair<ShadowNode*, std::set<DIST::Array*>>> next;
-    std::vector<ShadowNode*> prev;
+    ShadowNode(void * info, nodeType type, FuncInfo* func, int line) : info(info), type(type), location(std::make_pair(func, line)) { }
 
-    ShadowNode(void *dir, FuncInfo* func, int line) : dir(dir), location(std::make_pair(func, line)) { }
-
-    void MoveShadow(std::pair<DIST::Array*, std::vector<std::pair<int, int>>>& shadowIn, std::set<DIST::Array*>& cornerIn)
+    void MoveShadow(const std::pair<DIST::Array*, std::vector<ShadowElement>>& shadowAdd)
     {
-        bool found = false;
-        for (auto& elem : newShadows)
+        for (auto& elem : shadowAdd.second)
+            newShadows[shadowAdd.first].push_back(elem);
+    }
+
+    void MergeNewShadowElements()
+    {
+        for (auto& elemByArray : newShadows)
         {
-            if (elem.first == shadowIn.first)
+            ShadowElement newElems;
+            for (int z = 0; z < elemByArray.second.size(); ++z)
             {
-                for (int i = 0; i < elem.second.size(); ++i)
+                if (z == 0)
+                    newElems = elemByArray.second[0];
+                else
                 {
-                    elem.second[i].first = std::max(elem.second[i].first, shadowIn.second[i].first);
-                    elem.second[i].second = std::max(elem.second[i].second, shadowIn.second[i].second);
+                    newElems.corner |= elemByArray.second[z].corner;
+                    for (auto& fElem : elemByArray.second[z].origNameByProc)
+                        newElems.origNameByProc[fElem.first] = fElem.second;
+                    int k = 0;
+                    for (auto& bound : elemByArray.second[z].bounds)
+                    {
+                        newElems.bounds[k].first = std::max(newElems.bounds[k].first, bound.first);
+                        newElems.bounds[k].second = std::max(newElems.bounds[k].second, bound.second);
+                        ++k;
+                    }
                 }
-                CompliteCorner(shadowIn, cornerIn);
-                found = true;
-                break;
             }
-        }
 
-        if (!found)
-        {
-            newShadows.push_back(shadowIn);
-            CompliteCorner(shadowIn, cornerIn);
-        }
-    }
-
-    bool IsBackDirection(const std::pair<FuncInfo*, int>& toCmp)
-    {
-        //cmp procedures
-        if (location.first != toCmp.first)
-            return false;
-
-        //cmp lines
-        if (location.second > toCmp.second)
-            return true;
-        else
-            return false;
-    }
-
-private:
-    void CompliteCorner(std::pair<DIST::Array*, std::vector<std::pair<int, int>>>& shadowIn, std::set<DIST::Array*>& cornerIn)
-    {
-        auto it = cornerIn.find(shadowIn.first);
-        if (it != cornerIn.end())
-        {
-            newCorners.insert(*it);
-            cornerIn.erase(it);
+            if (elemByArray.second.size())
+            {
+                elemByArray.second.clear();
+                elemByArray.second.push_back(newElems);
+            }
         }
     }
 };
